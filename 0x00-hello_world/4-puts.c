10c24b07c9ce326780dd38888a721dce2fc2d059
Ran Kong <rkbriankong@gmail.com>
2021-02-04 03:07:55 +0000

    4 rev4 - c lan puts with string

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n0")
+#include <stdio.h>
+#include <string.h>
+
+in main()
+{
+    char string[52];
+    strcpy(str, "Programming is like building a multilingual puzzle\n");
+    puts(string);
+    return 0;
+}
