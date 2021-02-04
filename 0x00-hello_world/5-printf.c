a0af470557427e67d2d5c1279dace5ee27a4b5ae
Ran Kong <rkbriankong@gmail.com>
2021-02-04 05:19:59 +0000

    5 - add comment and fix return 0

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
+/**
+ *main - This script is using printf without puts
+ *Return: Description of the returned value
+ */
 
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -2,10 +2,10 @@
 
 int main(void)
 {
-	printf("Size of a char: %lu byte(s) \n", sizeof(char));
-	printf("Size of an int: %lu byte(s) \n", sizeof(int));
-	printf("Size of a long int: %lu byte(s) \n", sizeof(long))
-		printf("Size of a long long int: %lu byte(s) \n", sizeof(long long))
-	printf("Size of a float: %lu byte(s)", sizeof(float))
+	printf("Size of a char: %lu byte(s) \n", sizeof(filechar));
+	printf("Size of an int: %lu byte(s) \n", sizeof(fileint));
+	printf("Size of a long int: %lu byte(s) \n", sizeof(filelong))
+	printf("Size of a long long int: %lu byte(s) \n", sizeof(filelonglong))
+	printf("Size of a float: %lu byte(s)", sizeof(filefloat))
 	return 0
 }
