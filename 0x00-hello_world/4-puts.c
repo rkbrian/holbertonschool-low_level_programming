6d47a786dbc68d632fa5cd2eddc5af33a9ed9183
Ran Kong <rkbriankong@gmail.com>
2021-02-04 05:41:07 +0000

    6 - add comment and fix return 0 and fixed spacing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,16 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+ *main - This script uses puts and no pritf
+ *Return: value 0
+ */
+
 in main(void)
 {
-    char string[52];
-    strcpy(str, "Programming is like building a multilingual puzzle\n");
-    puts(string);
-    return 0;
+	char string[52];
+
+	strcpy(str, "Programming is like building a multilingual puzzle\n");
+	puts(string);
+	return (0);
 }
