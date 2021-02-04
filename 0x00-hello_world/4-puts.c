dcb3c22edeab516874304a940d020175947350ac
Ran Kong <rkbriankong@gmail.com>
2021-02-04 02:59:09 +0000

    4 rev2 - bin bash with puts return

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,3 @@
-#include <stdio.h>
-#include <string.h>
-
-int main(void)
-{
-    char string[52];
-    strcpy(str, "Programming is like building a multilingual puzzle\n");
-    puts(string);
-    return (0);
-}
+#!/bin/bash
+puts(str, "Programming is like building a multilingual puzzle\n");
+return (0);
