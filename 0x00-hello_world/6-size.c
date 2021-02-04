aa15725948173e4cb6ca76b05705a6a02dd3dc30
Ran Kong <rkbriankong@gmail.com>
2021-02-04 06:00:28 +0000

    6 - spacing and details

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 
+/**
+ *main - This script uses printf and sizeof
+ *Return: value 0
+ */
+
 int main(void)
 {
-	printf("Size of a char: %lu byte(s) \n", sizeof(filechar));
-	printf("Size of an int: %lu byte(s) \n", sizeof(fileint));
-	printf("Size of a long int: %lu byte(s) \n", sizeof(filelong))
-	printf("Size of a long long int: %lu byte(s) \n", sizeof(filelonglong))
-	printf("Size of a float: %lu byte(s)", sizeof(filefloat))
-	return 0
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long));
+	printf("Size of a long long int: %lu byte(s)\n", sizeof(long long));
+	printf("Size of a float: %lu byte(s)", sizeof(float));
+	return (0);
 }
