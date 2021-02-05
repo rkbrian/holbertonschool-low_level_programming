ea0fab5af2779ce0b77a9f097f580b7735f43770
Ran Kong <rkbriankong@gmail.com>
2021-02-05 23:54:29 +0000

    9-rev with correct if and putchar order

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - base 10 digit numbers with comma and space, using putchar only
- * Return: value 0
- **/
+ *main - base 10 digit numbers with comma and space, using putchar only
+ *Return: value 0
+ */
 
 int main(void)
 {
 	int d;
-
 
 	for (d = '0' ; d <= '9' ; d++)
 	{
