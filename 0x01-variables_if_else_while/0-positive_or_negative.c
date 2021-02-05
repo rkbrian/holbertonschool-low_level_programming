f2187eec0d80c981b4c7a93a5a57fef5537504d3
Ran Kong <rkbriankong@gmail.com>
2021-02-05 01:46:38 +0000

    0 added include stdioh

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,6 +1,6 @@
 #include <stdlib.h>
 #include <time.h>
-/* This code defines whether n is positive, negative or zero */
+#include <stdio.h>
 
 /**
  *main - whatever number is positive or negative
@@ -12,12 +12,12 @@ int main(void)
 	int n;
 
 	srand(time(0));
-	n = rBand() - RAND_MAX / 2;
+	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
-	printf("%d is negative\n", n);
+		printf("%d is negative\n", n);
 	else
-	printf("%d is zero\n", n);
+		printf("%d is zero\n", n);
 	return (0);
 }
---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+/* Last Digit */
+
+/**
+ *main - prints the last digit of number generated
+ *Return: 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int lastd
+	char str1[20] = "greater"
+	char str2[20] = "less"
+	scanf("%d %d %s %s", &n, &????)
+		printf("Last digit of %d is %d and is %s than %d", )
+	return (0);
+}
