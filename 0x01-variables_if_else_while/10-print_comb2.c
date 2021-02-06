c2cef775481bdcf42d35f2a52d3804172e37ff65
Ran Kong <rkbriankong@gmail.com>
2021-02-06 03:47:17 +0000

    9 and rev

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+/**
+ *main - 2-digit numbers with comma and space, using putchar only
+ *Return: value 0
+ */
+
+int main(void)
+{
+	int i, j;
+
+	for (i = '0' ; i <= '9' ; i++)
+	{
+		for (j = '0' ; j <= '9' ; j++)
+			{
+				putchar(i);
+				putchar(j);
+				if (i == '9' && j == '9')
+					continue;
+				else
+				putchar(',');
+				putchar(' ');
+			}
+
+	}
+	putchar('\n');
+
+	return (0);
+}
