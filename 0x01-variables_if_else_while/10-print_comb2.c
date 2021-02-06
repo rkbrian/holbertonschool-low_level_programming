e3f65212eba2274f202ed908cb8fc6c6d797b6e2
Ran Kong <rkbriankong@gmail.com>
2021-02-06 04:00:30 +0000

    9 re for betty

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -12,16 +12,15 @@ int main(void)
 	for (i = '0' ; i <= '9' ; i++)
 	{
 		for (j = '0' ; j <= '9' ; j++)
-			{
-				putchar(i);
-				putchar(j);
-				if (i == '9' && j == '9')
-					continue;
-				else
-				putchar(',');
-				putchar(' ');
-			}
-
+		{
+			putchar(i);
+			putchar(j);
+			if (i == '9' && j == '9')
+				continue;
+			else
+			putchar(',');
+			putchar(' ');
+		}
 	}
 	putchar('\n');
 
