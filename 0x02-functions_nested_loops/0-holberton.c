2b332438beb815f6141016d36e1335e328c278d8
Ran Kong <rkbriankong@gmail.com>
2021-02-08 21:47:29 +0000

    0-plus alias over git push lets do this

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,17 @@
+#include <stdio.h)
+#include <string.h>
+
+/**
+ * main - Use putchar to print word Holberton, no printf or puts.
+ * Return: 0
+ **/
+
+int main(void)
+{
+	int i;
+	char holberton = "Holberton\n";
+
+		for (i = 0 ; i < strlen(holberton) ; i++)
+			putchar(holberton[i];)
+				return (0);
+}
