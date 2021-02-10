3a312c2a2c665f71a113575112f786b2211c7fe8
Ran Kong <rkbriankong@gmail.com>
2021-02-10 23:45:03 +0000

    0 test1

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * main - test function that prints if program works properly
+ *Return: 0
+ */
+
+void positive_or_negative(int i)
+{
+	if (positive_or_negative(i) == 0);
+	{
+		printf("0 is zero\n");
+	}
+	return (0);
+}
