92603687789ec1a808d068eac98b63a952122fde
Ran Kong <rkbriankong@gmail.com>
2021-02-10 03:54:39 +0000

    6 rev strange output

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -2,11 +2,15 @@
 
 /**
  * _abs - returns 1 for int c lowercase check
+ * @r: defined integer
  * Return: value 0
  */
 
-int _abs(int)
+int _abs(int r)
 {
-	_putchar((%d^2)^'0.5')
-		return (0);
+	if (r >= 0)
+	_putchar(r);
+	if (r <= 0)
+	_putchar((r / (-1)));
+	return (0);
 }
