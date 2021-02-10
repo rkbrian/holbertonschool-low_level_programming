58a0193a8427d5b0856f847ccdc0a3f3b17017bc
Ran Kong <rkbriankong@gmail.com>
2021-02-10 05:33:34 +0000

    7 failed again

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,16 +3,10 @@
 /**
  * print_last_digit - prints the last digit of number generated
  * @i: input
- * @j: return
  * Return: 0
  */
 
-int print_last_digit(int j)
+int print_last_digit(int i)
 {
-	int i;	
-
-	while ((j = i % 10))
-	{
-		return j;
-	}
+	return (i % 10);
 }
