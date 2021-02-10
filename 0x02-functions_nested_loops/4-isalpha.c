b1f7326407c7e88176681b276bf7d6b25f2a88d5
Ran Kong <rkbriankong@gmail.com>
2021-02-10 01:41:46 +0000

    4

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,14 @@
 #include "holberton.h"
 
 /**
- * _islower - returns 1 for lowercase check
+ * _isalpha - returns 1 for letter check
+ * @c: character for the check
  * Return: value 0
  */
 
-int _islower(int c)
+int _isalpha(int c)
 {
-	if (c >= 'a' && c <= 'z')
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
