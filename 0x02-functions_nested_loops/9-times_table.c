fa5712d15390c69f0241751d9195065c395fa1a7
Ran Kong <rkbriankong@gmail.com>
2021-02-10 14:59:01 +0000

    9 test

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+/**
+ * times_table - 9x9
+ */
+
+void times_table(void)
+{
+	int i = '0', j = '0';
+	
+	while (i <= '9')
+	{
+		i++;
+		while (j <= '9')
+		{
+			return ((i * j));
+			j++;
+			if (j == '9')
+			continue;
+			else
+				_putchar(',');
+			_putchar(' ');
+		}
+	}
+}
