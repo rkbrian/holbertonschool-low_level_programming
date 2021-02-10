98e7c11b7219424a3ac564acdeeb6ab075a3e088
Ran Kong <rkbriankong@gmail.com>
2021-02-10 03:22:16 +0000

    skip5 and 6

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -9,14 +9,15 @@
 int print_sign(int n)
 {
 	if (n > '0')
-		_putchar('+');
-		return (1);
+	       	return (1);
+	_putchar('+');
 
 	if (n < '0')
-		_putchar('-');
-		return (-1);
+		return ('/');
+	_putchar('-');
 
 	if (n == '0')
-		_putchar('0');
+		return (0);
+	_putchar('0');
 	return (0);
 }
---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+
+/**
+ * _abs - returns 1 for int c lowercase check
+ * Return: value 0
+ */
+
+int _abs(int)
+{
+	_putchar((%d^2)^'0.5')
+		return (0);
+}
