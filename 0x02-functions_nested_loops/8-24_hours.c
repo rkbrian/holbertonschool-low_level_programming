dc15478a63bce272b2b744ee63029a414fd54a07
Ran Kong <rkbriankong@gmail.com>
2021-02-10 08:08:33 +0000

    8 test 2

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -6,28 +6,25 @@
 
 void jack_bauer(void)
 {
-	int i = 0, j, k, l;
+	int i = 0, j = 0, k = 0, l = 0;
 
-	while (i <= '2')
+	_putchar(i);
+	_putchar(j);
+	_putchar(':');
+	_putchar(k);
+	_putchar(l);
+	while (l <= '9')
 	{
-		j = '0';
-		while (j <= '3')
+		l++;
+		while (k <= '5')
 		{
-			k = '0';
-			while (k <= '5')
-			{
-				l = '0';
-				while (l <= '9')
-				_putchar(i);
-				_putchar(j);
-				_putchar(':');
-				_putchar(k);
-				_putchar(l);
-				l++;
-			}
 			k++;
+			while (k <= '3')
+			{
+				j++;
+				while (i <= '2')
+				i++;
+			}
 		}
-		j++;
 	}
-	i++;
 }
