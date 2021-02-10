545ab81261e87115f114141f6b3f2f589b080fe0
Ran Kong <rkbriankong@gmail.com>
2021-02-10 20:26:29 +0000

    9 finally with variable product int n and next j product nxt

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,29 +6,40 @@
 
 void times_table(void)
 {
-	int i, j;
+	int i, j, n, nxt;
 
 	for (i = 0 ; i <= 9 ; i++)
 	{
 		for (j = 0 ; j <= 9 ; j++)
 		{
-			_putchar(i * j);
+			n = i * j;
+			nxt = i * (j + 1);
+			if (n < 10)
+			{
+				_putchar(n + '0');
+			}
+			else
+			{
+			_putchar(n / 10 + '0');
+			_putchar(n % 10 + '0');
+			}
+
 			if (j == 9)
 			{
 				continue;
 			}
-			else if ((i * j) < 10)
+			else if (nxt >= 10)
 			{
 				_putchar(',');
-				_putchar(' ');
 				_putchar(' ');
 			}
 			else
 			{
 				_putchar(',');
 				_putchar(' ');
+				_putchar(' ');
 			}
-			_putchar('\n');
 		}
+		_putchar('\n');
 	}
 }
