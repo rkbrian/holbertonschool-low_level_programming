300f853bd848b58a5e3f29d4349fd3cadc65b262
Ran Kong <rkbriankong@gmail.com>
2021-02-10 05:56:06 +0000

    8 1st try

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -10,9 +10,9 @@ void jack_bauer(void)
 {
 	int i, j, k, l;
 
-	for (i = 0 ; i < 6 ; i++)
+	for (i = 0 ; i < 3 ; i++)
 	{
-		for (j = 0 ; j <= 9 ; j++)
+		for (j = 0 ; j <= 3 ; j++)
 		{
 			for (k = 0 ; k < 6 ; k++)
 			{
