4e295cf632cdda8be70c5f039d4387011cfaf38d
Ran Kong <rkbriankong@gmail.com>
2021-02-10 22:36:27 +0000

    11 test

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_to_98 - print all natual numbers in order, to 98
+ * @n: starting number
+ */
+
+void print_to_98(int n)
+{
+	printf("%d", n);
+	while (!(n = 98))
+	{
+		if (n < 98)
+		{
+		n++;
+		printf(", %d", n);
+		}
+		else if (n > 98)
+		{
+		n--;
+		printf(", %d", n);
+		}
+	}
+	_putchar('\n');
+}
