2b7d734823d9af5d97b70971004ff14676a390c2
Ran Kong <rkbriankong@gmail.com>
2021-02-11 19:43:35 +0000

    3 - 0 to 9 with new lines

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * print_numbers - to print integer 0 - 9 with new lines
+ */
+
+void print_numbers(void)
+{
+	int a;
+
+	for (a = 0 ; a <= 9 ; a++)
+	{
+		printf("%d\n", a);
+	}
+}
