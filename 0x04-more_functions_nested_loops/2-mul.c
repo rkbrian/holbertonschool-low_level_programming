01ee0df7eaeac0730a7b0a2153f83e64c615fa9f
Ran Kong <rkbriankong@gmail.com>
2021-02-11 19:32:03 +0000

    2 multiply-test

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * mul - function to multiply 2 integers
+ * @a: 1st integer
+ * @b: 2nd integer
+ * Return: 0
+ */
+
+int mul(int a, int b)
+{
+	printf("%d*%d", a, b);
+		return (0);
+}
