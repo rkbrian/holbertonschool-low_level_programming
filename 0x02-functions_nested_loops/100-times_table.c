f4e58d02a5aa631511b462fea1fc64e4eff581f0
Ran Kong <rkbriankong@gmail.com>
2021-02-11 06:43:47 +0000

    12 the nasty loop

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -2,6 +2,7 @@
 
 /**
  * print_times_table - any size of table starting w 0
+ * @n: input value of table size
  */
 
 void print_times_table(int n)
@@ -9,9 +10,7 @@ void print_times_table(int n)
 	int i, j, k, nxt;
 
 	if (n < 0 || n > 15)
-	{
-		continue;
-	}
+	putchar('\n');
 	else
 	{
 	for (i = 0 ; i <= n ; i++)
@@ -20,45 +19,18 @@ void print_times_table(int n)
 		{
 			k = i * j;
 			nxt = i * (j + 1);
-			if (k < 10)
-			{
-				_putchar(k + '0');
-			}
-			else if (k >= 10 && k < 100)
-			{
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
-			else
-			{
-				_putchar(k / 100 + '0');
-				_putchar(k / 10 + '0');
-				_putchar(k % 10 + '0');
-			}
+			printf("%d", k);
+
 			if (j == n)
-			{
 				continue;
-			}
 			else if (nxt >= 10 && nxt < 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-			}
+				printf(",  ");
 			else if (nxt >= 100)
-			{
-				_putchar(',');
-				_putchar(' ');
-			}
+				printf(", ");
 			else
-			{
-				_putchar(',');
-				_putchar(' ');
-				_putchar(' ');
-				_putchar(' ');
-			}
+				printf(",   ");
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 	}
 }
