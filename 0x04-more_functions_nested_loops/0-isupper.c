31ec2b799589a5d7eefdd62a9fd7d5eb66c030cd
Ran Kong <rkbriankong@gmail.com>
2021-02-11 16:30:01 +0000

    h

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+/* This will be my own header for any strange function used
+   in the directory, if any. */
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+int convert_day(int month, int day);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* HOLBERTON_H */
