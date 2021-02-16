09b9518f0aa5ab23deb505cc29b56a58479569e0
Ran Kong <rkbriankong@gmail.com>
2021-02-16 23:51:40 +0000

    2 test

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "holberton.h"
+
+/**
+ * _strlen - pointer to find string length
+ * @s: string input
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = &s;
+	_putchar(len + '0');
+	return (0);
+}
--- 0x05-pointers_arrays_strings/holberton.h
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -3,10 +3,14 @@
 
 #include <stdio.h>
 
-/* This will be my own header for any strange function used
-   in the directory, if any. */
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 
-int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
@@ -17,5 +21,7 @@ void puts2(char *str);
 void puts_half(char *str);
 void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
+int _putchar(char c);
+
 
 #endif /* HOLBERTON_H */
