af6e9e26b6944a2edf2941becd7a0c2122a722ff
Ran Kong <rkbriankong@gmail.com>
2021-02-16 06:24:21 +0000

    files

---
+++ 0x05-pointers_arrays_strings/holberton.h
@@ -0,0 +1,11 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+#include <stdio.h>
+
+/* This will be my own header for any strange function used
+   in the directory, if any. */
+
+int _putchar(char c);
+
+#endif
