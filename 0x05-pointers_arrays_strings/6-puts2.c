e6c70b86df1fbbff27086da2b5b51faab4eccd90
Ran Kong <rkbriankong@gmail.com>
2021-02-17 20:39:16 +0000

    6 rev for nothing beyond null byte

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,10 +9,12 @@ void puts2(char *str)
 {
 	int i = 0;
 
-	while (str[i] != '\0' && str[i + 1] != '\0')
+	for (; str[i] != '\0'; i++)
 	{
-		_putchar(str[i]);
-		i = i + 2;
+		if (i % 2 == 0)
+		{
+			_putchar(str[i]);
+		}
 	}
 	_putchar('\n');
 }
