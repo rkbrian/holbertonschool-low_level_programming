f0f36a9b207e2144c2c560733700fc1b830ae4e9
Ran Kong <rkbriankong@gmail.com>
2021-02-18 01:02:15 +0000

    8 rev for output range

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -8,8 +8,17 @@
 
 void print_array(int *a, int n)
 {
-	int i;
+	int i, j = 0;
 
+	while (a[j] != '\0')
+	{
+		j++;
+	}
+	j--;
+	if (j < n)
+	{
+		n = j;
+	}
 	for (i = 0 ; i < n ; i++)
 	{
 		if (i == n - 1)
