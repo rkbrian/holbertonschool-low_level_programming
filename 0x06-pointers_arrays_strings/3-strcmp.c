ed0e15fe7a1d2cbbb39289f32da2846650c6156a
rkbrian <rkbriankong@gmail.com>
2021-02-20 05:03:12 +0000

    3 rev

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,12 +9,14 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (*s1 == '\0' && *s2 == '\0')
+int i;
+  
+ for (i = 0 ; (s1[i] != '\0' && s2[i] != '\0') ; i++)
 	{
-		return (0);
+	  if (s1[i] != s2[i])
+	    {
+	  return (s1[i] - *s2[i]);
+	    }
 	}
-	else
-	{
-		return (*s1 - *s2);
-	}
+ return (0);
 }
