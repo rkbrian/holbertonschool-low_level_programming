130b6b827228bd819a7e18bde570dfc4dc5b42cc
rkbrian <rkbriankong@gmail.com>
2021-02-20 05:51:09 +0000

    7 rev

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,25 +1,25 @@
-#include <stdio.h>
-#include <string.h>
+#include "holberton.h"
 
 /**
  * leet - function to replace certain letters with num
- * @: output string or array
+ * @p: output string or array
  * Return: p
  */
 
 char *leet(char *p)
 {
-	int i = 0, j;
-	char l[30], u[30], *p = *;
+	int i, j;
+	char a[] = "aeotlAEOTL";
+	char q[] = "43071";
 
-	for (l[i] = 'a', u[i] = 'A' ; l[i] <= 'z' ; i++)
+	for (i = 0; p[i] != 0 ; i++)
 	{
-	}
-	for (j = 0 ; j <= i ; j++)
-	{
-		if ( *p == l[j])
+		for (j = 0; j < 5; j++)
 		{
-			*p = u[j];
+			if (p[i] == a[j] || p[i] == a[j + 5])
+			{
+				p[j] = q[j];
+			}
 		}
 	}
 	return (p);
