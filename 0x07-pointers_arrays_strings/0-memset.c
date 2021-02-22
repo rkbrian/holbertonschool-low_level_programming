6cb91155b6e7b9ec41775ce16619ff1be69c3a32
rkbrian <rkbriankong@gmail.com>
2021-02-22 21:11:10 +0000

    0 test

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -4,18 +4,19 @@
  * _memset - function to fill memory w const byte
  * @s: char string
  * @b: const char
+ * @n: first num of char to be replaced
  * Return: pointer to s
  */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i;
+	unsigned int i;
 	char **p;
 
 	for (i = 0; i < n ; i++)
 	{
 		s[i] = b;
-		p = s[0][i];
+		p[0][i] = s[i];
 	}
 	return (p);
 }
