e325a37423ab52f8f3dd47b2c0e7b5b2c27061c2
rkbrian <rkbriankong@gmail.com>
2021-02-23 04:27:33 +0000

    4 test

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -2,23 +2,25 @@
 
 /**
  * strpbrk - function to locate 1st appearence of accept
- *
+ * @s: char string
+ * @accept: string with target char
+ * Return: bytes before hitting the new char
  */
 
 char *_strpbrk(char *s, char *accept)
 {
+	unsigned int i, j;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+	for (j = 0 ; s[j] != '\0'; j++)
+	{
+		for (i = 0 ; accept[i] != '\0' ; i++)
+		{
+			if (s[j] == accept[i])
+			{
+				break;
+			}
+		}
+		break;
+	}
+	return (&s[i]);
 }
