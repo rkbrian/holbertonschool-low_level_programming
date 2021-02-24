9a90b2933a894884da1a6210c730ed45df27dbe2
rkbrian <rkbriankong@gmail.com>
2021-02-24 22:39:38 +0000

    1 with plus for moving out of the end of string to check backward

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * _print_rev_recursion - function to print reversed str w recursion
+ * @s: string
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
