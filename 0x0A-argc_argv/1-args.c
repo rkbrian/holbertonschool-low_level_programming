c964ef2a214ffb155b9537618b18c731b6ba9abd
rkbrian <rkbriankong@gmail.com>
2021-02-28 22:45:49 +0000

    1 test

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - print argument count
+ * @argc: argument count
+ * @argv: to be void
+ * Return: 0
+ */
+
+int main(int argc, char *argv[])
+{
+	(void)argv;
+	printf("%d\n", argc);
+	return (0);
+}
