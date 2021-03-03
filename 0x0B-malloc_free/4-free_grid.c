bdcfa5a203198555c610bdf39ea58bb335735188
rkbrian <rkbriankong@gmail.com>
2021-03-03 03:54:54 +0000

    3 rev 3 - plus 1 for null byte and free

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+#include <stdlib.h>
+
+/**
+ * alloc_grid - fuction to return ptr to a 2d array
+ * @width: width of 2d array
+ * @height: height of 2d array
+ *Return: the pointer
+ */
