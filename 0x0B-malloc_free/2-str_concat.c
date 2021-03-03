479545b9beb16ef0fa5027cf770bf2a1df312150
rkbrian <rkbriankong@gmail.com>
2021-03-03 00:37:31 +0000

    2 with some logic from question 1

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,6 +13,14 @@ char *str_concat(char *s1, char *s2)
 	int i, j, k;
 	char *cats;
 
+	if (s1 == NULL && s2 != NULL)
+	{
+		return (s2);
+	}
+	if (s2 == NULL && s1 != NULL)
+	{
+		return (s1);
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
