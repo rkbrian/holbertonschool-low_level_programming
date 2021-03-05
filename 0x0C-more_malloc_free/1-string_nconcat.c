f7cd1b19c7b7783fc0409e518c7fe369afdb6d2e
rkbrian <rkbriankong@gmail.com>
2021-03-05 00:30:05 +0000

    1 test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -5,7 +5,7 @@
  * string_nconcat - function to concatenate 2 strings
  * @s1: string 1
  * @s2: string 2
- * @n: string 2 limitation
+ * @n: string 2 limitation of bytes
  * Return: cat string or null
  */
 
@@ -25,7 +25,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
-	for (j = 0; s2[j] != '\0' && j <= n; j++)
+	for (j = 0; s2[j] != '\0' && j < (n / sizeof(char)); j++)
 	{
 	}
 	cats = malloc((i + j + 1) * sizeof(char));
