0ed5bade486608c6ad390f5006f85410d0566a03
rkbrian <rkbriankong@gmail.com>
2021-03-05 00:18:42 +0000

    1 test

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,9 +15,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *cats;
 
 	if (s1 == NULL)
-	{s1 = "";}
+	{
+		s1 = "";
+	}
 	if (s2 == NULL)
-	{s2 = "";}
+	{
+		s2 = "";
+	}
 	for (i = 0; s1[i] != '\0'; i++)
 	{
 	}
