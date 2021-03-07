89ad306ad221d51ac5e07aca3d64260f2fb67b4a
rkbrian <rkbriankong@gmail.com>
2021-03-07 20:04:20 +0000

    2 renew

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -14,16 +14,12 @@ char *_strncpy(char *dest, char *src, int n)
 
 	if (src == NULL)
 	{
-		return (NULL);
+		return (dest);
 	}
-	while (i < n)
+	while (i < n && dest[i] != '\0' && src[i] != '\0')
 	{
 		dest[i] = src[i];
 		i++;
-		if (src[i] == '\0')
-		{
-			break;
-		}
 	}
 	return (dest);
 }
