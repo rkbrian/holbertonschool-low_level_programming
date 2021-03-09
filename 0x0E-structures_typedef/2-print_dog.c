c42d0dde53e1554663d1ca9212e3642ac762e814
rkbrian <rkbriankong@gmail.com>
2021-03-09 05:48:08 +0000

    4 test

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -16,14 +16,7 @@ void print_dog(struct dog *d)
 	{
 		printf("Name: %s\n", d->name);
 	}
-	if (d->age == '\0')
-	{
-		printf("Age: 0.000000\n");
-	}
-	else
-	{
-		printf("Age: %.6f\n", d->age);
-	}
+	printf("Age: %.6f\n", d->age);
 	if (d->owner == NULL)
 	{
 		printf("Owner: (nil)\n");
--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -6,6 +6,7 @@
  * @name: string
  * @age: number
  * @owner: string
+ * Return: new dog or null
  */
 
 dog_t *new_dog(char *name, float age, char *owner)
