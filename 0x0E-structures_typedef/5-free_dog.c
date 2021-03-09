62888d6f808b06108d44586865405d5c5e8e75f0
rkbrian <rkbriankong@gmail.com>
2021-03-09 17:07:40 +0000

    5 rev 1

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,8 +1,8 @@
-#include "holberton.h"
+#include "dog.h"
 #include <stdlib.h>
 
 /**
- * free_dog - fuction to free malloc of ptr to a 2d array
+ * free_dog - fuction to free malloc of dog_t
  * @d: typedef of a struct
  */
 
@@ -10,8 +10,8 @@ void free_dog(dog_t *d)
 {
 	if (d != NULL)
 	{
-		free(d.name);
-		free(d.owner);
+		free(d->name);
+		free(d->owner);
 	}
 	free(d);
 }
