5c0561c00c53d548101e39ddcbaf2b30be314642
rkbrian <rkbriankong@gmail.com>
2021-03-09 03:57:16 +0000

    3 test

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,8 +16,19 @@ struct dog
 	float age;
 	char *owner;
 };
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 
+/**
+ * dog_t - new name for type struct dog
+ */
+
+typedef struct dog dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+};
 
 #endif
