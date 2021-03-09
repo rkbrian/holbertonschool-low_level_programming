63a5c36df00b394643367a88cddaf227198bb237
rkbrian <rkbriankong@gmail.com>
2021-03-09 17:18:31 +0000

    4 rev 4

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -24,6 +24,8 @@ dog_t *new_dog(char *name, float age, char *owner)
 	who = malloc(sizeof(struct dog));
 	if (who == NULL)
 	{
+		free(who->name);
+		free(who->owner);
 		free(who);
 		return (NULL);
 	}
