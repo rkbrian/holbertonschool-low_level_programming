6146769f1c00588022fe6ca984d483aa1d37472b
rkbrian <rkbriankong@gmail.com>
2021-03-09 05:41:31 +0000

    4 test

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,24 @@
+#include "dog.h"
+#include <stdlib.h>
+
+/**
+ * new_dog - function to create new dog
+ * @name: string
+ * @age: number
+ * @owner: string
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *whoof;
+
+	whoof = malloc(sizeof(struct dog));
+	if (whoof == NULL)
+	{
+		return (NULL);
+	}
+	whoof->name = name;
+	whoof->age = age;
+	whoof->owner = owner;
+	return (whoof);
+}
