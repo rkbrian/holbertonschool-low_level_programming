df9a804489aaf771ae87cb9b1fcf6c3f41475e9f
rkbrian <rkbriankong@gmail.com>
2021-03-10 21:46:05 +0000

    0 test

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function to print a name
+ * @name: input name string
+ * @f: pointer to function for the input
+ */
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,14 @@
+ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+#include <stdio.h>
+
+/**
+ * prototypes for function pointers
+ */
+
+void print_name(char *name, void (*f)(char *));
+
+
+
+#endif
