4f3fb4b3d6c2922eb7cab31553f6e0333f21b27d
rkbrian <rkbriankong@gmail.com>
2021-03-10 22:58:17 +0000

    0 rev 1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,20 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - function to exe a function
+ * @array: array
+ * @size: unsigned int type, size of array
+ * @action: ptr to the func needed
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (name != NULL)
+	{
+		f(name);
+	}
+	else
+	{
+		f("(nil)");
+	}
+}
