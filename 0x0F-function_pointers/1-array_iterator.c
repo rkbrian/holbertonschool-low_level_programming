9fddbdfd86e1f8002bb316dafe585ca000a32d35
rkbrian <rkbriankong@gmail.com>
2021-03-10 23:36:09 +0000

    1 rev 2 for func not null

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -11,7 +11,7 @@ void array_iterator(int *array, size_t size, void (*action)(int))
 {
 	unsigned int i;
 
-	if (array != NULL action != NULL && size > 0)
+	if (array != NULL && action != NULL && size > 0)
 	{
 		for (i = 0; i < size && array[i] != '\0'; i++)
 		{
