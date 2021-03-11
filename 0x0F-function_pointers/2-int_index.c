73c0296940070170005f4924cdd1ff072f420887
rkbrian <rkbriankong@gmail.com>
2021-03-11 00:59:43 +0000

    2 rev 2

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,21 +5,27 @@
  * @array: array
  * @size: size of array
  * @cmp: ptr to the func for comparing values
+ * @nn: input in cmp
  */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	unsigned int i;
+	int i = 0;
 
-	if (array != NULL || size > 0)
+	if (size <= 0)
 	{
-		for (i = 0; i < size; i++)
+		return (-1);
+	}
+	if (cmp != NULL && array != NULL)
+	{
+		while (i < size)
 		{
-			action(array[i]);
+			i++;
+			if (cmp(array[i]) != 0)
+			{
+				return (i);
+			}
 		}
 	}
-	else
-	{
-		NULL;
-	}
+	return (-1);
 }
