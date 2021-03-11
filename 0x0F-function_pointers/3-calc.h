93e7e9ddf2aea0f961a3a02cef0a3115a32811bd
rkbrian <rkbriankong@gmail.com>
2021-03-11 04:29:29 +0000

    3 rev 5

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -1,6 +1,7 @@
 #ifndef CALC_H
 #define CALC_H
 
+#include <stdlib.h>
 #include <stdio.h>
 
 /**
@@ -20,6 +21,5 @@ int op_mul(int a, int b);
 int op_div(int a, int b);
 int op_mod(int a, int b);
 int (*get_op_func(char *s))(int, int);
-int main(int argc, char *argv[]);
 
 #endif
