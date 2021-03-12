cc3c89528dbf3cfed8808aae17fdd2164aa69f8b
rkbrian <rkbriankong@gmail.com>
2021-03-12 00:49:12 +0000

    100 test

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * print_all - function to print anything
+ * @format: list of types of arguments passed to the function
+ */
+
+void print_all(const char * const format, ...)
+{
+	char i;
+	va_list everything;
+
+	va_start(everything, format);
+
+	while()
+	{
+
+
+	}
+	printf("");
+	va_end(everthing);
+}
