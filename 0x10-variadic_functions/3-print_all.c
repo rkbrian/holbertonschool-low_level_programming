4134fed65577320bfd8dafbdc5ee64a4ae387b34
rkbrian <rkbriankong@gmail.com>
2021-03-12 04:48:01 +0000

    just for saving progress on 3

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -9,16 +9,38 @@
 
 void print_all(const char * const format, ...)
 {
-	char i;
+	unsigned int i = 0, j = 0;
+	char *str;
+	char letar[4][2] = {
+		{"c", 'char'},
+		{"i", 'int'},
+		{"f", 'float'},
+		{"s", 'char*'},
+	};
 	va_list everything;
 
 	va_start(everything, format);
 
-	while()
+	if (format == NULL)
+	{
+		printf("(nil)");
+	}
+	printf("%s", str);
+	while (format[i] != '\0')
 	{
 
-
+		while (letar[j][0] == format[i] && j < 4)
+		{
+			str = va_arg(everthing, letar[j][1]);
+			if (str)
+			{
+				printf(", %s", str);
+			}
+			j++;
+		}
+		i++;
 	}
-	printf("");
+	printf("\n");
+
 	va_end(everthing);
 }
