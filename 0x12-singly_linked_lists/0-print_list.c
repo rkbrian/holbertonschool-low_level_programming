2b55235610513073dd134de7b579f5541f6546e4
rkbrian <rkbriankong@gmail.com>
2021-03-18 22:16:19 +0000

    0 progress saving

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -1,1 +1,23 @@
-#include "list.h"
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ *
+ *
+ */
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+	int strlen;
+
+	while (h != NULL)
+	{
+		for (strlen = 0; h[strlen] != '\0'; strlen++)
+		{
+		}
+		printf("[%d] %s\n", strlen, h);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
