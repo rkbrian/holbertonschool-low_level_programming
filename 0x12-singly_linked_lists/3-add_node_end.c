1929a7497bf9217c60284de10c41955d8690e183
rkbrian <rkbriankong@gmail.com>
2021-03-19 21:27:54 +0000

    3 revs 8

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,32 +9,57 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newend;
-	size_t count;
+	list_t *newend, *tmp;
 
-	if (head == NULL || str == NULL)
-	{
+	if (str == NULL)
 		return (NULL);
+	if (*head == NULL)
+	{
+		newend = malloc(sizeof(list_t));
+		if (newend == NULL)
+			return (NULL);
+		newend->str = strdup(str);
+		if (newend->str == NULL)
+		{
+			free(newend);
+			return (NULL);
+		}
+		newend->len = _count(newend->str);
+		newend->next = NULL;
+		*head = newend;
+		return (newend);
 	}
 	newend = malloc(sizeof(list_t));
 	if (newend == NULL)
-	{
 		return (NULL);
-	}
 	newend->str = strdup(str);
 	if (newend->str == NULL)
 	{
 		free(newend);
 		return (NULL);
 	}
-	for (count = 0; newend->str[count] != '\0'; count++)
-	{
-	}
-	newend->len = count;
-	if (head == NULL)
-	{
-		*head = newend;
-	}
+	newend->len = _count(newend->str);
 	newend->next = NULL;
+	tmp = *head; /* switching start */
+	while (tmp->next != NULL)
+	{
+		tmp = tmp->next; /* direction changed */
+	}
+	tmp->next = newend; /* ending of switch */
 	return (newend);
+}
+
+/**
+ * _count - string length
+ * @s: string input
+ * Return: string length
+ */
+size_t _count(char *s)
+{
+	size_t i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+	}
+	return (i);
 }
