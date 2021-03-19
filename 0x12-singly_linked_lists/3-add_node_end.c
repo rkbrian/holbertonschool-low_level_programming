0a74c7c0efe4ee8ed7dfc7f0049b0b1a1f672479
rkbrian <rkbriankong@gmail.com>
2021-03-19 03:59:55 +0000

    3 failed rev and 4 test

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -9,29 +9,32 @@
  */
 list_t *add_node_end(list_t **head, const char *str)
 {
-	list_t *newhead;
+	list_t *newend;
 	size_t count;
 
 	if (head == NULL || str == NULL)
 	{
 		return (NULL);
 	}
-	newhead = malloc(sizeof(list_t));
-	if (newhead == NULL)
+	newend = malloc(sizeof(list_t));
+	if (newend == NULL)
 	{
-		free(newhead);
 		return (NULL);
 	}
-	newhead->str = strdup(str);
-	if (newhead->str == NULL)
+	newend->str = strdup(str);
+	if (newend->str == NULL)
 	{
-		free(newhead->str);
+		free(newend);
 		return (NULL);
 	}
-	for (count = 0; newhead->str[count] != '\0'; count++)
+	for (count = 0; newend->str[count] != '\0'; count++)
 	{
 	}
-	newhead->len = count;
-	newhead->next = NULL;
-	return (newhead);
+	newend->len = count;
+	newend->next = NULL;
+	(*head == NULL)
+	{
+		*head = newend;
+	}
+	return (newend);
 }
---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - fuction to free malloc of struct
+ * @head: struct and first node
+ */
+
+void free_list(list_t *head)
+{
+	free(*head->str);
+	free(*head->len);
+	free(*head->next);
+	if (*head != NULL)
+	{
+		free(*head);
+	}
+}
