e864bfe35e71fa32879829defbdf5a3d24a055ab
rkbrian <rkbriankong@gmail.com>
2021-03-19 00:11:40 +0000

    0 test

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -2,8 +2,9 @@
 #include "lists.h"
 
 /**
- *
- *
+ * print_list - function to print linked list content and node count
+ * @h: input struct, linked list
+ * Return: node count
  */
 size_t print_list(const list_t *h)
 {
@@ -12,10 +13,16 @@ size_t print_list(const list_t *h)
 
 	while (h != NULL)
 	{
-		for (strlen = 0; h[strlen] != '\0'; strlen++)
+		if (h->str == NULL)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			i++;
+		}
+		for (strlen = 0; h->str[strlen] != '\0'; strlen++)
 		{
 		}
-		printf("[%d] %s\n", strlen, h);
+		printf("[%d] %s\n", strlen, h->str);
 		h = h->next;
 		i++;
 	}
