0d527ac31dc6c11387aa17a96125f94340765a79
rkbrian <rkbriankong@gmail.com>
2021-03-23 02:13:46 +0000

    0 test

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * print_listint - function to print linked list content and node count
+ * @h: input struct, linked list
+ * Return: node count
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	size_t i;
+
+	if (h == NULL)
+	{
+		return (1);
+	}
+	for (i = 1; h->next != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	printf("%d\n", h->n);
+	return (i);
+}
