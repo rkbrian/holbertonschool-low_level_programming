eede3af8c602f413f1e434c9c1d509fdf55a22a5
rkbrian <rkbriankong@gmail.com>
2021-03-24 20:03:12 +0000

    10 rev 4

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -24,7 +24,7 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = tmp;
 		return (1);
 	}
-	for (i = 0; i < index && newtmp->next != NULL; i++)
+	for (i = 0; i < index && tmp->next != NULL; i++)
 	{
 		newtmp = tmp;
 		tmp = tmp->next;
