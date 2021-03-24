96559d517864497bb945fd86931fb89064fcbd28
rkbrian <rkbriankong@gmail.com>
2021-03-24 07:36:46 +0000

    7 test 2

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,6 +12,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 	listint_t *tmp = head;
 	unsigned int i;
 
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 	for (i = 0; i < index; i++)
 	{
 		tmp = tmp->next;
