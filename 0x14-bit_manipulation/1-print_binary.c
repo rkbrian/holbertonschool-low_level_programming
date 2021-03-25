34a581db18e26a592a79044b72b6ded85e82b883
rkbrian <rkbriankong@gmail.com>
2021-03-25 23:58:56 +0000

    1 test

---
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include "holberton.h"
+
+/**
+ * print_binary - function that prints the binary representation of a number
+ * @n: number input
+ */
+
+void print_binary(unsigned long int n)
+{
+	unsigned long int i = 1, j, k = 1, subnum, num;
+
+	while (k < _powertool(n))
+	{
+		k = k * 2;
+	}
+	j = n - k;
+	while (j)
+	{
+		subnum = 1;
+		while (i < j)
+		{
+			subnum = subnum << 1;
+			i = i * 2;
+		}
+		num = num + subnum;
+		j = j - _powertool(j);
+	}
+	_putchar(num + '0');
+}
+
+unsigned long int _powertool(unsigned long int a)
+{
+	unsigned long int b = 1;
+
+	while ((b * 2) < a)
+	{
+		b = b * 2;
+	}
+	return (b);
+}
--- 0x14-bit_manipulation/holberton.h
+++ 0x14-bit_manipulation/holberton.h
@@ -13,5 +13,7 @@ int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
 int clear_bit(unsigned long int *n, unsigned int index);
 unsigned int flip_bits(unsigned long int n, unsigned long int m);
+int _putchar(char c);
+unsigned long int _powertool(unsigned long int a);
 
 #endif
