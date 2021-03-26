16f3864d22ea94b3c2740b5c88466078f41e1df9
rkbrian <rkbriankong@gmail.com>
2021-03-26 02:45:30 +0000

    1 rev 98

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,11 +8,15 @@
 
 void print_binary(unsigned long int n)
 {
+	unsigned long int ld;
+/* last decimal digit switch for the 1st binary digit */
+
 	if (n > 1)
 	{
 		print_binary(n >> 1);
 	}
-	_putchar((n & 1) + '0');
+	ld = n & 1;
+	_putchar(ld + '0');
 }
 
 /*	unsigned long int i = 1, j, k = 1, l, subnum, num = 0;
