fbff3be5b1d7b01fc802bf0e6b80391367ccd039
rkbrian <rkbriankong@gmail.com>
2021-03-26 04:55:07 +0000

    5 rev 2

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -6,13 +6,12 @@
  to another
  * @n: 1st number input
  * @m: 2nd number input
- * @index: starting from 0 of the bit to get
- * Return: 1 if succeed or -1 if failedthe num of bits
+ * Return: the num of bits
  */
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned int i = 0, num = 0;
+	unsigned int i, num = 0;
 
 	i = n ^ m;
 	if (i == 0)
