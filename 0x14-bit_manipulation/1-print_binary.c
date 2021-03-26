f43bc25e39a0fe9829eae7e63a8c16280a588e70
rkbrian <rkbriankong@gmail.com>
2021-03-26 02:52:44 +0000

    1 rev 98

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,8 +8,7 @@
 
 void print_binary(unsigned long int n)
 {
-	unsigned long int ld;
-/* last decimal digit switch for the 1st binary digit */
+	unsigned long int ld;/* last digit switch for the 1st binary digit */
 
 	if (n > 1)
 	{
@@ -18,46 +17,3 @@ void print_binary(unsigned long int n)
 	ld = n & 1;
 	_putchar(ld + '0');
 }
-
-/*
- * unsigned long int i = 1, j, k = 1, l, subnum, num = 0;
-	if (n == 0)
-		_putchar('0');
-	else
-	{
-		l = _powertool(n);
-		subnum = 1;
-	while (k < l)
-	{
-		k = k << 1;
-		subnum = subnum * 10;
-	}
-	num = num + subnum;
-	j = n - k;
-	while (j > 0)
-	{
-		subnum = 1;
-		while (i * 2 < j)
-		{
-			subnum = subnum * 10;
-			i = i << 1;
-		}
-		i = 1;
-		num = num + subnum;
-		j = j - _powertool(j);
-	}
-	_putchar(num + '0');
-	}
-}
-
-unsigned long int _powertool(unsigned long int a)
-{
-	unsigned long int b = 1;
-
-	while ((b * 2) < a)
-	{
-		b = b << 1;
-	}
-	return (b);
-}
-*/
