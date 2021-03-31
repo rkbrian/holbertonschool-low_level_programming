bfdfef572f344c2f5337cbc767b27a0b59a373cc
rkbrian <rkbriankong@gmail.com>
2021-03-31 05:40:31 +0000

    1 rev 2

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,22 +8,50 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int fd, i, len, wri;
+	char *buffalo;
 
 	if (filename == NULL)
 	{
 		return (-1);
 	}
-	fd = open(filename, O_CREAT | O_WRONLY, 0600);
-	if (fd < 0)
+	for (i = 0; text_content[i] != '\0'; i++)
 	{
+	}
+	buffalo = malloc(sizeof(char) * i + 1);
+	if (buffalo == NULL)
+	{
+		free(buffalo);
+		return (-1);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+	{
+		free(buffalo);
 		return (-1);
 	}
 	if (text_content == NULL)
 	{
+		free(buffalo);
+		close(fd);
 		return (1);
 	}
+	len = read(fd, buffalo, i);
+	if (len == -1)
+	{
+		free(buffalo);
+		close(fd);
+		return (-1);
+	}
+	wri = write(STDOUT_FILENO, buffalo, len);
+	if (wri == -1)
+	{
+		free(buffalo);
+		close(fd);
+		return (-1);
+	}
 	dprintf(fd, "%s", text_content);
+	free(buffalo);
 	close(fd);
 	return (1);
 }
