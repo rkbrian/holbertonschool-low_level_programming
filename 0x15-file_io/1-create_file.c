ef7d076e694c4bbf72329f13cfda104c50fb41f5
rkbrian <rkbriankong@gmail.com>
2021-03-31 15:42:13 +0000

    1 rev 3

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,6 +9,7 @@
 int create_file(const char *filename, char *text_content)
 {
 	int fd;
+	ssize_t wri, i;
 
 	if (filename == NULL)
 	{
@@ -24,7 +25,14 @@ int create_file(const char *filename, char *text_content)
 		close(fd);
 		return (1);
 	}
-	dprintf(fd, "%s", text_content);
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+	}
+	wri = write(fd, text_content, i);
+	if (wri == -1)
+	{
+		return (-1);
+	}
 	close(fd);
 	return (1);
 }
