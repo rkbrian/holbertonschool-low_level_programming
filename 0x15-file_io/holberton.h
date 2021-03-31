a8622231dead69c14b91badfb02040b43bb3aae8
rkbrian <rkbriankong@gmail.com>
2021-03-31 03:44:18 +0000

    0 test

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -7,7 +7,10 @@
 #include <sys/stat.h>
 #include <unistd.h>
 /**
- *
+ * read_textfile - function to read text file and print it to the POSIX stdout
+ * @filename: file name
+ * @letters: allowable count of letters
+ * Return: length of file text or 0 if failed
  */
 ssize_t read_textfile(const char *filename, size_t letters);
 
