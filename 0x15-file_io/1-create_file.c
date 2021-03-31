d689f597129dd832039ce09b67d5ee8a17776fe4
rkbrian <rkbriankong@gmail.com>
2021-03-31 04:42:35 +0000

    1 test

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,55 @@
+#include "holberton.h"
+
+/**
+ * create_file - function to create a file
+ * @filename: file name
+ * @text_content: text content for the fille
+ * Return: 1 if succeeded or 0 if failed
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, i, len, wri;
+	char *buffalo;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; text_content[i] != '\0'; i++)
+	{
+	}
+	buffalo = malloc(sizeof(char) * i + 1);
+	if (buffalo == NULL)
+	{
+		free(buffalo);
+		return (0);
+	}
+	fd = open(filename, O_CREAT | O_WRONLY, 0600);
+	if (fd == -1)
+	{
+		free(buffalo);
+		return (0);
+	}
+	if (text_content == NULL)
+	{
+		free(buffalo);
+		return (1);
+	}
+	len = read(fd, buffalo, i);
+	if (len == -1)
+	{
+		free(buffalo);
+		close(fd);
+		return (0);
+	}
+	wri = write(STDOUT_FILENO, buffalo, len);
+	if (wri == -1)
+	{
+		free(buffalo);
+		close(fd);
+		return (0);
+	}
+	free(buffalo);
+	close(fd);
+	return (len);
+}
--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -13,9 +13,8 @@
  * Return: length of file text or 0 if failed
  */
 ssize_t read_textfile(const char *filename, size_t letters);
-
-
-
+int create_file(const char *filename, char *text_content);
+int append_text_to_file(const char *filename, char *text_content);
 
 
 
