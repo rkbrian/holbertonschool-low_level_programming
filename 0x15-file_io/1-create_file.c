ae74b365a9fe43b24056e7f2fca69025c2f0ef94
rkbrian <rkbriankong@gmail.com>
2021-03-31 05:32:31 +0000

    1 rev 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,9 +15,9 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	fd = open(filename, O_CREAT | O_WRONLY, 0600);
-	if (fd == -1)
+	if (fd < 0)
 	{
-		return (0);
+		return (-1);
 	}
 	if (text_content == NULL)
 	{
