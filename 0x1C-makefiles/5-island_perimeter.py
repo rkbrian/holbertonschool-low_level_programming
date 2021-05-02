0931e1d31bd80c5a28456952cbb324920192eaa8
rkbrian <rkbriankong@gmail.com>
2021-05-02 22:56:36 +0000

    5 fixed!

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -7,8 +7,8 @@ def island_perimeter(grid):
 
     The grid contains island with no lake, but possibly has bay.
     It is considered island beause it is not attaching the border.
-    endrow: the end of row, should be less than 100
-    endcol: the end of column, should be less than 100
+    endr: the end of row, should be less than 100
+    endc: the end of column, should be less than 100
     west_coast: length count of the direction facing coast
     east_coast: length count of the direction facing coast
     north_coast: length count of the direction facing coast
@@ -16,8 +16,8 @@ def island_perimeter(grid):
     return: return perimeter
     """
 
-    endrow = len(grid)
-    endcol = len(grid[0])
+    endr = len(grid)
+    endc = len(grid[0])
     west_coast = 0
     east_coast = 0
     north_coast = 0
@@ -25,15 +25,15 @@ def island_perimeter(grid):
     if grid is None:
         return 0
     else:
-        for i in range(0, endrow):
-            for j in range(0, endcol):
+        for i in range(0, endr):
+            for j in range(0, endc):
                 if grid[i][j] == 1 and (i - 1 < 0 or grid[i - 1][j] == 0):
                     west_coast += 1
-                if grid[i][j] == 1 and (i + 1 >= endrow or grid[i + 1][j] == 0):
+                if grid[i][j] == 1 and (i + 1 >= endr or grid[i + 1][j] == 0):
                     east_coast += 1
                 if grid[i][j] == 1 and (j - 1 < 0 or grid[i][j - 1] == 0):
                     north_coast += 1
-                if grid[i][j] == 1 and (j + 1 >= endcol or grid[i][j + 1] == 0):
+                if grid[i][j] == 1 and (j + 1 >= endc or grid[i][j + 1] == 0):
                     south_coast += 1
         perimeter = west_coast + east_coast + north_coast + south_coast
         return perimeter
