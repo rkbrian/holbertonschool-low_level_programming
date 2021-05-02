1e1b7ca124c6d11501a511872e5616f6d329028f
rkbrian <rkbriankong@gmail.com>
2021-05-02 22:21:30 +0000

    5 rev 4

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -26,9 +26,9 @@ def island_perimeter(grid):
 
     if grid is None:
         return 0
-    elif endrow < 100 and endcol < 100:
-        for i in range(1, endrow - 1):
-            for j in range(1, endcol - 1):
+    else:
+        for i in range(1, endrow):
+            for j in range(1, endcol):
                 if grid[i][j] == 1 and grid[i - 1][j] == 0:
                     west_coast += 1
                 if grid[i][j] == 1 and grid[i + 1][j] == 0:
