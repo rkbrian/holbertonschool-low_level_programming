b192aa549c837e641c868e7a8846d9d5f46122d0
rkbrian <rkbriankong@gmail.com>
2021-07-21 20:40:19 +0000

    setup

---
+++ 0x00-hash_tables/0-hash_table_create.c
@@ -0,0 +1,21 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - function to create hash table
+ * @size: hash table size in integer
+ * Return: hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i = 0;
+	hash_node_t **array;
+
+	while (i < size)
+	{
+		;
+		hash_table_create = hash_table_create->next;
+		i++;
+	}
+	return ();
+}
