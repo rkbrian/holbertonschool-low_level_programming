13aae531c87d88629222267e33ce314576b0ce26
Ran Kong <rkbriankong@gmail.com>
2022-03-12 22:48:00 -0600

    interpolation search: test

--- 0x02-search_algorithms/102-interpolation.c
+++ 0x02-search_algorithms/102-interpolation.c
@@ -1,7 +1,8 @@
 #include "search_algos.h"
 
 /**
- * interpolation_search - 
+ * interpolation_search - searche for a value in a sorted array of integers
+ *  using the Interpolation search algorithm
  * @array: pointer to the first element of the sorted array to search in
  * @size: the number of elements in array
  * @value: the value to search for
